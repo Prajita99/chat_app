b9a5e3e2787b52782f81a83569c2166e2f4435ca
Prajita Adhikari <prajitaadhikari2001@gmail.com>
2023-09-06 12:28:53 +0545

    added chat features

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:chat_app/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 final _firestore = FirebaseFirestore.instance;
 late User loggedInUser; //late is used to defer initialization
 
 class ChatScreen extends StatefulWidget {
-
   static const String id = 'chat_screen';
 
   @override
@@ -18,23 +18,24 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  String ?messageText;
+  String? messageText;
+  Timestamp? time;
 
   void getCurrentUser() async {
-    try{
-  final user = await _auth.currentUser;
-  if (user != null) {
-    loggedInUser = user;
+    try {
+      final user = await _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
   }
-  } catch (e){
-    print(e);
-  }
-}
 
   @override
   void initState() {
     super.initState();
-    getCurrentUser(); 
+    getCurrentUser();
   }
 
   @override
@@ -76,11 +77,12 @@ class _ChatScreenState extends State<ChatScreen> {
                   TextButton(
                     onPressed: () {
                       messageTextController.clear();
-                      _firestore.collection('messages').add({
-                        'text': messageText,
-                        'sender': loggedInUser.email,
-                        'timestamp': FieldValue.serverTimestamp(),
-                      });
+                      _firestore.collection('messages')
+                        ..add({
+                          'text': messageText,
+                          'sender': loggedInUser.email,
+                          'timestamp': FieldValue.serverTimestamp(),
+                        });
                     },
                     child: Text(
                       'Send',
@@ -101,85 +103,106 @@ class MessagesStream extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('messages').orderBy('timestamp').snapshots(),
-              builder: (context, snapshot) { //this napshot is not same as above snapshot(above one was a querySnapshot and this one is flutter's async snapshot that makes use of query snapshot)
-                if(snapshot.hasData) {
-                  final messages = snapshot.data!.docs;
-                  List<MessageBubble> messageBubbles = [];
-                  for (var message in messages) {
-                    final messageData = message.data() as Map<String, dynamic>;
-                    final messageText = messageData['text'] as String;
-                    final messageSender = messageData['sender'] as String;
-                    final timestamp = messageData['timestamp'] as Timestamp?;
+      stream:
+          _firestore.collection('messages').orderBy('timestamp').snapshots(),
+      builder: (context, snapshot) {
+        //this napshot is not same as above snapshot(above one was a querySnapshot and this one is flutter's async snapshot that makes use of query snapshot)
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final messages = snapshot.data!.docs.reversed;
+        List<MessageBubble> messageBubbles = [];
+        for (var message in messages) {
+          final messageData = message.data() as Map<String, dynamic>;
+          final messageText = messageData['text'] as String;
+          final messageSender = messageData['sender'] as String;
+          final time = messageData['timestamp'] as Timestamp;
 
-                     if (timestamp != null) {
-                        // Use timestamp
-                      } else {
-                        // Handle case where timestamp is null
-                      }
+          if (time != null) {
+            final dateTime = time.toDate();
+            final messageText = message['text'];
 
-                    final currentUser = loggedInUser.email;
-
-                    final messageBubble = MessageBubble(
-                      sender: messageSender,
-                      text: messageText,
-                      timestamp: timestamp,
-                      me: currentUser == messageSender,
-                    );
-                    messageBubbles.add(messageBubble);
-                  }
-                  return Expanded(
-                    child: ListView(
-                      reverse: true, //to scroll to latest message
-                      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-                      children: messageBubbles,
-                    ),
-                  );
-                }
-                else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  return CircularProgressIndicator(); // Placeholder widget when loading
-                }
-              },
+            // You can format the timestamp to display it nicely
+            final formattedTimestamp = DateFormat('HH:mm').format(dateTime);
+          } else{
+            final errorMessageWidget = MessageBubble(
+    
+               
             );
+          }
+
+          final currentUser = loggedInUser.email;
+
+          final messageBubble = MessageBubble(
+            sender: messageSender,
+            text: messageText,
+            me: currentUser == messageSender,
+            timestamp: time,
+          );
+          messageBubbles.add(messageBubble);
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true, //to scroll to latest message
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
   }
 }
 
 class MessageBubble extends StatelessWidget {
   MessageBubble({this.sender, this.text, this.me, this.timestamp});
 
-  final String ?sender;
-  final String ?text;
-  final Timestamp ?timestamp;
-  final bool ?me;
+  final String? sender;
+  final String? text;
+  final bool? me;
+  final Timestamp? timestamp;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
-          crossAxisAlignment: me! ? CrossAxisAlignment.end : CrossAxisAlignment.start, // Adjusted CrossAxisAlignment based on 'me'
+        crossAxisAlignment: me!
+            ? CrossAxisAlignment.end
+            : CrossAxisAlignment
+                .start, // Adjusted CrossAxisAlignment based on 'me'
         children: [
           Text(
             sender!,
             style: TextStyle(
               fontSize: 12.0,
               color: Colors.black54,
-            ),),
+            ),
+          ),
           Material(
-            borderRadius: me! ? BorderRadius.only(topLeft: Radius.circular(30.0), bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0)) : BorderRadius.only(topRight: Radius.circular(30.0), bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0)),
+            borderRadius: me!
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0))
+                : BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0)),
             elevation: 5.0,
-            color: me! ? Colors.lightBlueAccent: Color.fromARGB(255, 217, 212, 212) ,
+            color: me!
+                ? Colors.lightBlueAccent
+                : Color.fromARGB(255, 217, 212, 212),
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
               child: Text(
-                '$text',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    color: Colors.white
-                  ),
-                ),
+                text!,
+                style: TextStyle(fontSize: 15.0, color: Colors.white),
+              ),
             ),
           ),
         ],
